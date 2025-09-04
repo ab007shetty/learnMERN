030238d465d67ff399dba55b9a6d3933dc4206d4
ab007shetty <ab007shetty@users.noreply.github.com>
2025-09-04 18:07:25 +0000

    deploy: 9fbd2e4ec46822190a100224a8cfd9890106934c

---
+++ assets/Counter-CC-4huJl.js
@@ -0,0 +1,19 @@
+import{j as e}from"./index-DME0ChcM.js";import{P as o}from"./PlaygroundWrapper-C8RUvC-h.js";const n=`
+//Write React components, JSX, or pure JS. No need of any import statements.
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+// setCounter(counter + 1) can be wrong, use prevCount to always get the latest value
+
+  return (
+    <div>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}> - </button>
+      <h1> {counter} </h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}> + </button>
+    </div>
+  );
+}
+
+export default Counter;
+`.trim();function s(t){return e.jsx(o,{defaultCode:n,...t})}export{s as default};
---
+++ assets/Factorial-CATmjv2F.js
@@ -0,0 +1,41 @@
+import{j as e}from"./index-DME0ChcM.js";import{P as o}from"./PlaygroundWrapper-C8RUvC-h.js";const a=`
+//Write React components, JSX, or pure JS. No need of any import statements.
+
+function Factorial() {
+  const [input, setInput] = useState("3");
+  const [output, setOutput] = useState("");
+
+  const calculateFactorial = () => {
+    const number = parseInt(input);
+    let factorial = 1;
+    for (let i = 1; i <= number; i++) {
+      factorial *= i;
+    }
+    setOutput(factorial);
+  };
+
+  return (
+    <div>
+      <input value={input} onChange={e => setInput(e.target.value)} />
+      <button onClick={calculateFactorial}>Factorial</button>
+      <div>{output}</div>
+    </div>
+  );
+}
+
+export default Factorial;
+`.trim(),r=`
+//Write React components, JSX, or pure JS. No need of any import statements.
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+// Example usage:
+console.log(factorial(5)); // 120
+console.log(factorial(0)); // 1
+`.trim();function i(t){return e.jsx(o,{defaultCode:a,defaultJsCode:r,...t})}export{i as default};
